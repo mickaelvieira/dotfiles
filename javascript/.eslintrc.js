e93838727747c1a25cbe5c62d11792664654391f
Mickael Vieira <contact@mickael-vieira.com>
2017-02-23 16:37:58 +0000

    Add eslint configuration

---
+++ javascript/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+      'comma-dangle': ['error', {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      }],
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      'react/jsx-curly-spacing': ['error', 'always', { allowMultiline: true }],
+      'react/prefer-stateless-function': ['error', { ignorePureComponents: true }],
+    },
+    "env": {
+        "browser": true
+    }
+};
