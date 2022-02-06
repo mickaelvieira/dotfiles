ac6ca3426766a8fc1f66b9034d0d0afece5bc760
Mickael Vieira <mickael@mickaelvieira.com>
2022-02-06 11:40:56 +0100

    remove unused docker images

--- docker/composer/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-php -v
-composer --ansi --version
-composer --ansi global config --list
-echo -e "\x1b[32mCache: \x1b[33m${COMPOSER_HOME}\x1b[0m"
-echo -e "\x1b[32mCwd: \x1b[33m$(pwd -P)\x1b[0m"
-
-exit 0
--- docker/yarn/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo -e "\x1b[32mYarn \x1b[33m$(yarn version)\x1b[0m"
-echo -e "\x1b[32mNode \x1b[33m$(node -v)\x1b[0m"
-echo -e "\x1b[32m$(sass -v)\x1b[0m"
-echo -e "\x1b[32mCache: \x1b[33m$(yarn cache dir)\x1b[0m"
-echo -e "\x1b[32mCwd: \x1b[33m$(pwd -P)\x1b[0m"
-
-exit 0
